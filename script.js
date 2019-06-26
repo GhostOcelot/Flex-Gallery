8987fe30ac3daff23cc919a5a41144f0499ddbc2
Houseplayer <lukasz.szymczyk@gmail.com>
2019-06-26 18:40:46 +0200

    popup gallery added, side chevrons added, chevrons not working

---
+++ script.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector(".wrapper");
+const smallImages = document.querySelectorAll(".imgSmall");
+const bigImages = document.querySelectorAll(".imgBig");
+
+for (let i = 0; i < smallImages.length; i++) {
+  smallImages[i].addEventListener("click", function () {
+    wrapper.classList.add("show");
+    bigImages[i].classList.toggle("show");
+  })
+}
+
+wrapper.addEventListener("click", function () {
+  this.classList.remove("show");
+  document.querySelector(".imgBig.show").classList.remove("show");
+
+})
